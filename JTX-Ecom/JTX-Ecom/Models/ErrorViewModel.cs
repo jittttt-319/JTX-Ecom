a12d3d03ae4f3a0344ca71c23ef309cbf8f41a94
hehe <jitxuan2021@gmail.com>
2025-11-07 11:12:12 +0800

    new

---
+++ JTX-Ecom/JTX-Ecom/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JTX_Ecom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
